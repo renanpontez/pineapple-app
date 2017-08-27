39a27d5663e1392d1dde78baa8606eda5dd59465
Renan <renanpontez@gmail.com>
2017-08-27 17:45:16 -0300

    small fix!

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -1,5 +1,5 @@
 module.exports = {
-    index: function(req, res) {
+    index: function(req, res, next) {
         Product.find().populate('ProductType').exec(function(err, result){
             if(err) return next(err);
 
